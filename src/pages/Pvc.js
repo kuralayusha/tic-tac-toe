32c4487e6c435ad731103be09b9b29ae4fccbb48
kurlayusha <tbark.guard@gmail.com>
2022-11-11 16:38:34 +0300

    Board setup, select square functions done

--- src/pages/Pvc.js
+++ src/pages/Pvc.js
@@ -1,7 +1,22 @@
 function Pvc() {
   return (
     <div>
-      <h1>PVC page</h1>
+      {/* TODO: en üstte -icon -sıraKimde -baştan başlama
+      orta bölümde 3x3 oyun alanı
+      alt bölümde -1.oyuncuSkoru -beraberlik -CPUSkoru*/}
+      <div className="topBar">
+        {/* icon */}
+        {/* sıra kimde */}
+        {/* baştan başlama */}
+      </div>
+
+      <div className="gameArea">{/* 3x3 oyun alanı */}</div>
+
+      <div className="scoreBoard">
+        {/* 1.oyuncuSkoru */}
+        {/* beraberlikSkoru */}
+        {/* CPUSkoru */}
+      </div>
     </div>
   )
 }
--- src/pages/Pvp.js
+++ src/pages/Pvp.js
@@ -1,7 +1,60 @@
-function Pvp() {
+import { useState } from 'react'
+import Square from './Square'
+
+function Pvp({ playerOneIcon, setPage }) {
+  const [squares, setSquares] = useState([
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ])
+  const [isX, setIsX] = useState(playerOneIcon === 'X' ? true : false)
+
+  function handleClick(i) {
+    squares[i] = isX ? 'X' : 'O'
+    setSquares(squares)
+    setIsX(!isX)
+  }
+
   return (
     <div>
-      <h1>PVP page</h1>
+      {/* TODO: en üstte -icon -sıraKimde -baştanBaşlama
+      orta bölümde 3x3 oyun alanı
+      alt bölümde -1.oyuncuSkoru -beraberlikSkoru -2.oyuncuSkoru*/}
+      <div className="topBar">
+        {/* icon */}
+        {/* sıra kimde */}
+        {/* baştan başlama */}
+      </div>
+
+      <div className="gameBoard">
+        <div className="board--row">
+          <Square value={squares[0]} onClick={() => handleClick(0)} />
+          <Square value={squares[1]} onClick={() => handleClick(1)} />
+          <Square value={squares[2]} onClick={() => handleClick(2)} />
+        </div>
+        <div className="board--row">
+          <Square value={squares[3]} onClick={() => handleClick(3)} />
+          <Square value={squares[4]} onClick={() => handleClick(4)} />
+          <Square value={squares[5]} onClick={() => handleClick(5)} />
+        </div>
+        <div className="board--row">
+          <Square value={squares[6]} onClick={() => handleClick(6)} />
+          <Square value={squares[7]} onClick={() => handleClick(7)} />
+          <Square value={squares[8]} onClick={() => handleClick(8)} />
+        </div>
+      </div>
+
+      <div className="scoreBoard">
+        {/* 1.oyuncuSkoru */}
+        {/* beraberlikSkoru */}
+        {/* 2.oyuncuSkoru */}
+      </div>
     </div>
   )
 }
